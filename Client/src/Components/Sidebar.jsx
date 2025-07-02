0e19c40ecf12e35ebb171b8064f8e6d5b4a06073
Dheerendra Vikram Dixit <dheerendradixit321@gmail.com>
2025-07-02 07:18:39 +0530

    dashboard icon added

--- Client/src/Components/Sidebar.jsx
+++ Client/src/Components/Sidebar.jsx
@@ -1,78 +1,3 @@
-// import { Box, Chip, ListItemText, MenuItem, MenuList, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-
-
-// const Sidebar = () => {
-//     const theme = useTheme();
-//   const isMobile = useMediaQuery(theme.breakpoints.down('sm','md'));
-//   const Links = [
-//     {
-//       name: "WishList",
-//       link: 'wishlist',
-//     },
-//     {
-//       name: 'MyOrders',
-//       link: 'myOrders',
-//     },
-
-//     {
-//       name: 'BooksListed',
-//       link: 'booksListed',
-//     },
-//   ];
-
-//   return (
-//     !isMobile?(
-//       <Box sx={{ width: 240, px: 2 }}>
-//       <Typography variant="h6" fontWeight="bold" mt={2} mb={1} color="primary">
-//         Dashboard Menu
-//       </Typography>
-
-//       <Stack spacing={1}>
-//         {Links.map((link, index) => (
-//           <MenuList key={index} sx={{ p: 0 }}>
-//             <MenuItem
-//               component={Link}
-//               to={link.link}
-//               sx={{
-//                 borderRadius: 1,
-//                 px: 2,
-//                 py: 1,
-//                 fontWeight: 500,
-//                 color: 'text.primary',
-//                 '&:hover': {
-//                   backgroundColor: '#e3f2fd',
-//                 },
-//               }}
-//             >
-//               <ListItemText>{link.name}</ListItemText>
-
-//               {/* ✅ Optional: Chip for Confirm Order */}
-//               {link.showChip && (
-//                 <Chip
-//                   label={link.chipCount}
-//                   size="small"
-//                   color="secondary"
-//                   sx={{ ml: 1 }}
-//                 />
-//               )}
-//             </MenuItem>
-//           </MenuList>
-//         ))}
-//       </Stack>
-//     </Box>
-//     ):false
-
-//   );
-// };
-
-// export default Sidebar;
-
-
-
-
 
 import {
   Box,
@@ -91,7 +16,7 @@ import {
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { KeyboardArrowRight } from '@mui/icons-material';
+import { DashboardCustomizeOutlined} from '@mui/icons-material';
 
 const Sidebar = () => {
   const theme = useTheme();
@@ -186,7 +111,7 @@ const Sidebar = () => {
             },
           }}
         >
-          <KeyboardArrowRight/>
+          <DashboardCustomizeOutlined/>
         </IconButton>
       )}
 
